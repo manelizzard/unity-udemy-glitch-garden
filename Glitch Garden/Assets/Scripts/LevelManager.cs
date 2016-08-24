f83c2551f69fd46687a397b5222c8f7b8e7a1285
Manel Simon <manelizzard@gmail.com>
2016-08-24 11:28:13 +0200

    ## Section 7, Lecture 129 * Created Splash Screen with automated load start menu * Imported assets

---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start() {
+		Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void LoadNextLevel() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+}
